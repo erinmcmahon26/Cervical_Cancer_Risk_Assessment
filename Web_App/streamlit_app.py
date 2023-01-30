6ca1018ff9cce8dbc5dea09a96f03f80de465487
Mona Ascha <mona@Monas-MacBook-Pro.local>
2023-01-30 17:11:18 -0500

    Added text, added new tab for images, added risk assessment info

--- Web_App/streamlit_app.py
+++ Web_App/streamlit_app.py
@@ -9,13 +9,19 @@ from PIL import Image
 image = Image.open('Web_App/VividHealth_Logo.png')
 st.image(image)
 
-tab1, tab2, tab3, tab4, tab5 = st.tabs(["Home", "About Us", "Risk Assessment", "Find A Provider", "Sign In"])
+tab1, tab2, tab3, tab4, tab5, tab6 = st.tabs(["Home", "About Us", "Risk Assessment", "Find A Provider", "Image Upload", "Sign In"])
 
 with tab1:
-    st.header("Will add something here soon")
+    st.header("Welcome")
+    st.markdown("Hundreds of thousands of women are diagnosed with cervical cancer in the United States every year. Even when not fatal, 88% of patients experience potentially debilitating long-term effects including fertility problems, fatigue, and urinary/bowel issues (Health Europa 2018). This adds up to billions in healthcare costs annually, despite advances in detection and prevention which have made cervical cancer one of the most preventable forms of cancer (National Cancer Institute 2022). While incidence rates in the United States initially plummeted with the introduction of vaccinations and screenings, rates have stagnated in the past decade (Figure 1). This stagnation is avoidable – the CDC estimates that even today 93% of cervical cancer cases are preventable (CDC 2020). This speaks to a need to decrease mortality by improving diagnosis, screening rates, and screening accessibility. VividHealth proposes to address this gap by introducing an algorithmic risk assessment tool that can be integrated into electronic medical record (EMR) software. The tool will serve three types of users: medical providers, current patients, and prospective/new patients. The provider view will allow medical professionals to upload images from cervical cancer screenings into our trained models to help diagnose precancerous or cancerous cells. We will display these results in conjunction with demographic risk factors from a patient’s chart in an intuitive cervical cancer risk analysis dashboard. The patient view will show the risk assessment results, guidelines, and any messages added by their providers. Finally, the tool will also have a public-facing feature so anyone can find providers near them who can perform cervical cancer screenings. Our tool will first be focused on the United States with the intention of expanding it to other countries. We believe that it is necessary to provide the healthcare community with more tools to efficiently and successfully perform their jobs and provide the public with easily accessible information in one place.")
 
 with tab2:
     st.header("About Us")
+    st.markdown("VividHealth is a leading medical technology consulting firm that focuses on improving patient outcomes by incorporating new technologies into existing healthcare systems to aid with diagnosis, prevention, and outreach. "
+                "We have partnered with Epic, an EMR company, to add new diagnostic capabilities into Epic’s Comprehensive Health Record software. "
+                "We are passionate about delivering diagnostic information to assist clinical judgment and building interactive tools to enable patients to take control of their health. "
+                "We created VividHealth out of our desire to make healthcare more accessible and cost effective. "
+                "Having a positive impact in our community and helping others obtain necessary care motivates us to continue our current work. ")
     image_M = Image.open('Web_App/Mona.png')
     col1, mid, col2 = st.columns([1, 3, 20])
     with col1:
@@ -86,18 +92,74 @@ with tab3:
 
     # Risk factor tool
     st.subheader("Risk Factor Calculator")
+
+    #Age
     age_min = st.number_input("Age")
     if age_min < 21:
         st.error("You are too young to use this tool!")
     else:
         st.success("You are old enough to use this tool!")
 
+    #Smoking status
     selected_class = st.radio("Smoking Status", ['Current smoker', 'Former Smoker', 'Never Smoker'])
     st.write("Smoking Status:", selected_class)
     # st.write("Smoking Status Type:", type(selected_class))
 
+    #Number of Years Smoked
+    #The following should only appear if smoking status is current smoker or former smoker...need to write if statement
+    smoke_years = st.number_input("Number of Years Smoking")
+    if smoke_years < 0:
+        st.error("Cannot be a negative number!")
+
+    #Number of Packs Smoked per day
+    ppd = st.number_input("Number of Packs Smoked Per Day")
+    if ppd < 0:
+        st.error("Cannot be a negative number!")
+
+    #Pack Years calculator
+    #Pack years = number of packs of cigarettes smoked per day multiplied by the number of years the person has smoked
+    pack_years = ppd*smoke_years
+    st.number_input("Pack Years",
+                    value=pack_years, #Defaults to pack years
+                    disabled=True) #User can't change it
+
+    #HPV
     selected_class = st.radio("History of HPV", ['Yes', 'No'])
     st.write("History of HPV:", selected_class)
+
+    #IUD
+    selected_class = st.radio("IUD Present", ['Yes', 'No'])
+    st.write("IUD Present:", selected_class)
+
+    #IUD Years
+    iud_years = st.number_input("Number of Years with IUD")
+    if iud_years < 0:
+        st.error("Cannot be a negative number!")
+
+    #Age at first sexual intercourse
+    age_first_sex = st.number_input("Age at First Sexual Intercourse")
+    if age_first_sex < 0:
+        st.error("Cannot be a negative number!")
+
+    #Contraception years
+    contracept_years = st.number_input("Number of Years with Contraception")
+    if contracept_years < 0:
+        st.error("Cannot be a negative number!")
+
+    #Number of sexual partners
+    num_sex_partners = st.number_input("Number of Lifetime Sexual Partners")
+    if num_sex_partners < 0:
+        st.error("Cannot be a negative number!")
+
+    #Number of pregnancies
+    num_pregnancies = st.number_input("Number of Lifetime Pregnancies")
+    if num_pregnancies < 0:
+        st.error("Cannot be a negative number!")
+
+    #Number of STDs
+    num_stds = st.number_input("Number of Lifetime STDs")
+    if num_stds < 0:
+        st.error("Cannot be a negative number!")
 
 with tab4:
     st.header("Find A Healthcare Provider Near You")
@@ -108,6 +170,25 @@ with tab4:
     st.map()
 
 with tab5:
+    st.header("Upload an Image to Assess Cervical Cancer Risk")
+    uploaded_file = st.file_uploader("Choose a file",
+                                     type=None,
+                                     accept_multiple_files=False, #only accept one file at a time
+                                     key=None,
+                                     help=None,
+                                     on_change=None,
+                                     disabled=False,
+                                     label_visibility="visible")
+    if uploaded_file is not None:
+        bytes_data = uploaded_file.getvalue()
+        # To read file as bytes:
+        st.write("filename:", uploaded_file.name)
+        st.write(bytes_data)
+        # Can be used wherever a "file-like" object is accepted:
+        dataframe = pd.read_csv(uploaded_file)
+        st.write(dataframe)
+
+with tab6:
     st.header("Patient and Healthcare Sign In")
 
 # selected_class = st.radio("Select User", ['Provider', 'Current Patient', 'New Patient'])
